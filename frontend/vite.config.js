47efdb1cb2c53d77b07c3d4828055eb8ee21dc9d
Dainampally Akshay <162695626+Dainampally-Akshay18@users.noreply.github.com>
2026-01-28 16:15:24 +0530

    Update API proxy target in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: env.VITE_API_URL,
+        target: "https://resumetrix.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
